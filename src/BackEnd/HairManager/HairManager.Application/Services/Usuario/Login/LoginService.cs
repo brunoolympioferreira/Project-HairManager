8363ad7ee50a9dd0db08287b120d0a5e2b37f464
Bruno Ferreira <bruno.olympio.ferreira@gmail.com>
2022-12-29 09:48:06 -0300

    feat: criando regra de negocio de fazer login

---
+++ src/BackEnd/HairManager/HairManager.Application/Services/Usuario/Login/LoginService.cs
@@ -0,0 +1,43 @@
+﻿using HairManager.Application.Utils.Criptografia;
+using HairManager.Application.Utils.Token;
+using HairManager.Comunication.Requests;
+using HairManager.Comunication.Responses;
+using HairManager.Domain.Repositories;
+using HairManager.Exceptions.ExceptionsBase;
+
+namespace HairManager.Application.Services.Usuario.Login;
+public class LoginService : ILoginService
+{
+    private readonly IUsuarioReadOnlyRepository _usuarioRepository;
+    private readonly EncriptadorDeSenha _encriptadorSenha;
+    private readonly TokenController _tokenController;
+
+    public LoginService(IUsuarioReadOnlyRepository usuarioRepository, EncriptadorDeSenha encriptadorSenha, TokenController tokenController)
+    {
+        _usuarioRepository = usuarioRepository;
+        _encriptadorSenha = encriptadorSenha;
+        _tokenController = tokenController;
+    }
+    public async Task<ResponseLoginDTO> Executar(RequestLoginDTO request)
+    {
+        var senhaCriptografada = _encriptadorSenha.CriptografarSenha(request.Senha);
+
+        var usuario = await _usuarioRepository.RecuperarUsuarioPorEmailESenha(request.Email, senhaCriptografada);
+
+        Validar(usuario);
+
+        return new ResponseLoginDTO
+        {
+            Nome = usuario.Nome,
+            Token = _tokenController.GerarToken(usuario.Email)
+        };
+    }
+
+    private static void Validar(Domain.Entities.Usuario usuario)
+    {
+        if (usuario == null)
+        {
+            throw new LoginInvalidoException();
+        }
+    }
+}
---
+++ src/BackEnd/Shared/HairManager.Comunication/Responses/ResponseLoginDTO.cs
@@ -0,0 +1,6 @@
+﻿namespace HairManager.Comunication.Responses;
+public class ResponseLoginDTO
+{
+    public string Nome { get; set; }
+    public string Token { get; set; }
+}
