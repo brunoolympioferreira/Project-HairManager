6bc9f3df6ee61e5e6177f1d88e648b7c7b5615db
Bruno Ferreira <bruno.olympio.ferreira@gmail.com>
2023-01-12 16:07:56 -0300

    feat: implementando testes de unidade recuperar perfil usuario

---
+++ tests/UtilsForTests/UsuarioLogado/UsuarioLogadoBuilder.cs
@@ -0,0 +1,32 @@
+﻿using HairManager.Application.Utils.UsuarioLogado;
+using Moq;
+
+namespace UtilsForTests.UsuarioLogado;
+public class UsuarioLogadoBuilder
+{
+    private static UsuarioLogadoBuilder _instance;
+    private readonly Mock<IUsuarioLogado> _repository;
+
+    public UsuarioLogadoBuilder()
+    {
+        _repository = new Mock<IUsuarioLogado>();
+    }
+
+    public static UsuarioLogadoBuilder Instancia()
+    {
+        _instance = new UsuarioLogadoBuilder();
+        return _instance;
+    }
+
+    public UsuarioLogadoBuilder RecuperarUsuario(HairManager.Domain.Entities.Usuario usuario)
+    {
+        _repository.Setup(c => c.RecuperarUsuario()).ReturnsAsync(usuario);
+
+        return this;
+    }
+
+    public IUsuarioLogado Construir()
+    {
+        return _repository.Object;
+    }
+}
