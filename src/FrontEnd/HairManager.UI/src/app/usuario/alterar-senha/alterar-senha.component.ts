6243fdf8f859dcbc64e70d0b36810b90ab57f0e2
Bruno Ferreira <bruno.olympio.ferreira@gmail.com>
2023-02-15 09:34:35 -0300

    feat: implementando tela alterar senha e validacoes

--- src/FrontEnd/HairManager.UI/src/app/usuario/alterar-senha/alterar-senha.component.ts
+++ src/FrontEnd/HairManager.UI/src/app/usuario/alterar-senha/alterar-senha.component.ts
@@ -1,9 +1,78 @@
-import { Component } from '@angular/core';
+//Angular
+import { ActivatedRoute, Router } from '@angular/router';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+import { AfterViewInit, Component, ElementRef, OnInit, ViewChildren } from '@angular/core';
+import { FormControlName } from '@angular/forms';
+//Utils
+import { ToastrService } from 'ngx-toastr';
+import { FormBaseComponent } from 'src/app/base/form-base-component';
+import { Usuario } from '../models/usuario';
+import { UsuarioService } from '../services/usuario.service';
+import { CustomValidators } from '@narik/custom-validators';
 
 @Component({
   selector: 'app-alterar-senha',
   templateUrl: './alterar-senha.component.html'
 })
-export class AlterarSenhaComponent {
+export class AlterarSenhaComponent extends FormBaseComponent implements OnInit, AfterViewInit {
+  @ViewChildren(FormControlName, { read: ElementRef }) formInputElements: ElementRef[];
 
+  errors: any[] = [];
+  alterarSenhaForm: FormGroup;
+  usuario: Usuario;
+
+  returnUrl: string;
+
+  constructor(
+    private fb: FormBuilder,
+    private usuarioService: UsuarioService,
+    private router: Router,
+    private route: ActivatedRoute,
+    private toastr: ToastrService) {
+    super();
+
+    this.validationMessages = {
+      senhaAtual: {
+        required: 'Informe a senha atual',
+        rangeLength: 'A senha deve possuir entre 6 e 15 caracteres'
+      },
+      novaSenha: {
+        required: 'Informe a nova senha',
+        rangeLength: 'A senha deve possuir entre 6 e 15 caracteres'
+      },
+      confirmeNovaSenha: {
+        required: 'Informe a nova senha',
+        rangeLength: 'A senha deve possuir entre 6 e 15 caracteres',
+        equalTo: 'As senhas não conferem'
+      }
+    };
+    super.configurarMensagensValidacaoBase(this.validationMessages);
+  }
+
+  ngOnInit(): void {
+    let password = new FormControl('', [Validators.required, CustomValidators.rangeLength([6, 15])]);
+    let confirmPassword = new FormControl('', [Validators.required, CustomValidators.rangeLength([6, 15]), CustomValidators.equalTo(password)]);
+
+    this.alterarSenhaForm = this.fb.group({
+      senhaAtual: ['', [Validators.required]],
+      novaSenha: password,
+      confirmeNovaSenha: confirmPassword
+    });
+  }
+
+  ngAfterViewInit(): void {
+    super.configurarValidacaoFormularioBase(this.formInputElements, this.alterarSenhaForm)
+  }
+
+  alterarSenha() {
+
+  }
+
+  processarSucesso() {
+
+  }
+
+  processarFalha() {
+
+  }
 }
