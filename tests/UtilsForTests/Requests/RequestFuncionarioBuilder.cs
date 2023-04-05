cfef8c2b677f3eeadd874c352365d1d9ecc24102
Bruno Ferreira <bruno.olympio.ferreira@gmail.com>
2023-04-04 21:27:15 -0300

    feat: implementando validators test funcionario mais endereco

---
+++ tests/UtilsForTests/Requests/RequestFuncionarioBuilder.cs
@@ -0,0 +1,44 @@
+﻿using Bogus;
+using Bogus.Extensions.Brazil;
+using HairManager.Comunication.DTO;
+using HairManager.Comunication.Enums;
+using HairManager.Comunication.Requests;
+using System;
+
+namespace UtilsForTests.Requests;
+public class RequestFuncionarioBuilder
+{
+    public static RequestAdicionarFuncionarioDTO Construir()
+    {
+        return new Faker<RequestAdicionarFuncionarioDTO>()
+            .RuleFor(c => c.Nome, f => f.Person.FullName)
+            .RuleFor(c => c.Telefone, f => f.Phone.PhoneNumber("############"))
+            .RuleFor(c => c.DataNascimento, f => f.Date.Past(60, DateTime.Now.AddYears(-16)))
+            .RuleFor(c => c.Nacionalidade, "Brasileiro")
+            .RuleFor(c => c.Endereco, Endereco())
+            .RuleFor(c => c.CTPSNumero, "2525252525")
+            .RuleFor(c => c.CTPSSerie, f => "3654")
+            .RuleFor(c => c.CPF, f => f.Person.Cpf(false))
+            .RuleFor(c => c.RG, f => "30998355")
+            .RuleFor(c => c.PIS, f => "1023589748")
+            .RuleFor(c => c.Reservista,"25871456987")
+            .RuleFor(c => c.Cargo, f => "Cabelereiro")
+            .RuleFor(c => c.Salario, 1500.55m)
+            .RuleFor(c => c.EstadoCivil, f => f.Random.Enum<EstadoCivilEnum>())
+            .RuleFor(c => c.DataAdmissao, f => f.Date.Recent())
+            .RuleFor(c => c.StatusFuncionario, f => f.Random.Enum<StatusFuncionarioEnum>());
+    }
+
+    private static EnderecoDTO Endereco()
+    {
+        return new Faker<EnderecoDTO>()
+            .RuleFor(e => e.Rua, f => f.Address.StreetName())
+            .RuleFor(e => e.Numero, "1000")
+            .RuleFor(e => e.Numero, "1000")
+            .RuleFor(e => e.Complemento, f => f.Lorem.Text())
+            .RuleFor(e => e.Bairro, f => f.Address.County())
+            .RuleFor(e => e.Cidade, f => f.Address.City())
+            .RuleFor(e => e.Estado, f => f.Random.Enum<EstadosEnum>())
+            .RuleFor(e => e.Pais, f => f.Address.Country());
+    }
+}
---
+++ tests/Validators.Test/Funcionario/Adicionar/AdicionarFuncionarioValidatorTest.cs
@@ -0,0 +1,274 @@
+﻿using FluentAssertions;
+using HairManager.Application.Services.Endereco;
+using HairManager.Application.Services.Funcionario;
+using HairManager.Exceptions.ExceptionsBase;
+using UtilsForTests.Requests;
+using Xunit;
+
+namespace Validators.Test.Funcionario.Adicionar;
+public class AdicionarFuncionarioValidatorTest
+{
+    [Fact]
+    public void Validar_Sucesso()
+    {
+        var validator = new AdicionarFuncionarioValidator();
+
+        var request = RequestFuncionarioBuilder.Construir();
+
+        var result = validator.Validate(request);
+
+        result.IsValid.Should().BeTrue();
+    }
+
+    [Fact]
+    public void Validar_Erro_Nome_EMBRANCO()
+    {
+        var validator = new AdicionarFuncionarioValidator();
+
+        var request = RequestFuncionarioBuilder.Construir();
+        request.Nome = string.Empty;
+
+        var result = validator.Validate(request);
+
+        result.IsValid.Should().BeFalse();
+        result.Errors.Should().ContainSingle().And.Contain(error => error.ErrorMessage.Equals(ResourceMensagensDeErro.NOME_FUNCIONARIO_EMBRANCO));
+    }
+
+    [Fact]
+    public void Validar_Erro_Nome_Invalido()
+    {
+        var validator = new AdicionarFuncionarioValidator();
+
+        var request = RequestFuncionarioBuilder.Construir();
+        request.Nome = "Morbi a metus. Phasellus enim erat, vestibulum vel, aliquam a,";
+
+        var result = validator.Validate(request);
+
+        result.IsValid.Should().BeFalse();
+        result.Errors.Should().ContainSingle().And.Contain(error => error.ErrorMessage.Equals(ResourceMensagensDeErro.NOME_FUNCIONARIO_INVALIDO));
+    }
+
+    [Fact]
+    public void Validar_Erro_Telefone_EmBranco()
+    {
+        var validator = new AdicionarFuncionarioValidator();
+
+        var request = RequestFuncionarioBuilder.Construir();
+        request.Telefone = string.Empty;
+
+        var result = validator.Validate(request);
+
+        result.IsValid.Should().BeFalse();
+        result.Errors.Should().ContainSingle().And.Contain(error => error.ErrorMessage.Equals(ResourceMensagensDeErro.TELEFONE_FUNCIONARIO_EMBRANCO));
+    }
+
+    [Fact]
+    public void Validar_Erro_Telefone_Invalido()
+    {
+        var validator = new AdicionarFuncionarioValidator();
+
+        var request = RequestFuncionarioBuilder.Construir();
+        request.Telefone = "1234";
+
+        var result = validator.Validate(request);
+
+        result.IsValid.Should().BeFalse();
+        result.Errors.Should().ContainSingle().And.Contain(error => error.ErrorMessage.Equals(ResourceMensagensDeErro.TELEFONE_FUNCIONARIO_INVALIDO));
+    }
+
+    [Fact]
+    public void Validar_Erro_Nacionalidade_EmBranco()
+    {
+        var validator = new AdicionarFuncionarioValidator();
+
+        var request = RequestFuncionarioBuilder.Construir();
+        request.Nacionalidade = string.Empty;
+
+        var result = validator.Validate(request);
+
+        result.IsValid.Should().BeFalse();
+        result.Errors.Should().ContainSingle().And.Contain(error => error.ErrorMessage.Equals(ResourceMensagensDeErro.NACIONALIDADE_EMBRANCO));
+    }
+
+    [Fact]
+    public void Validar_Erro_CTPS_Numero_EmBranco()
+    {
+        var validator = new AdicionarFuncionarioValidator();
+
+        var request = RequestFuncionarioBuilder.Construir();
+        request.CTPSNumero = string.Empty;
+
+        var result = validator.Validate(request);
+
+        result.IsValid.Should().BeFalse();
+        result.Errors.Should().ContainSingle().And.Contain(error => error.ErrorMessage.Equals(ResourceMensagensDeErro.CTPS_NUMERO_EMBRANCO));
+    }
+
+    [Fact]
+    public void Validar_Erro_CTPS_Serie_EmBranco()
+    {
+        var validator = new AdicionarFuncionarioValidator();
+
+        var request = RequestFuncionarioBuilder.Construir();
+        request.CTPSSerie = string.Empty;
+
+        var result = validator.Validate(request);
+
+        result.IsValid.Should().BeFalse();
+        result.Errors.Should().ContainSingle().And.Contain(error => error.ErrorMessage.Equals(ResourceMensagensDeErro.CTPS_SERIE_EMBRANCO));
+    }
+
+    [Fact]
+    public void Validar_Erro_CPF_Invalido()
+    {
+        var validator = new AdicionarFuncionarioValidator();
+
+        var request = RequestFuncionarioBuilder.Construir();
+        request.CPF = "12345678910";
+
+        var result = validator.Validate(request);
+
+        result.IsValid.Should().BeFalse();
+        result.Errors.Should().ContainSingle().And.Contain(error => error.ErrorMessage.Equals(ResourceMensagensDeErro.CPF_INVALIDO));
+    }
+
+    [Fact]
+    public void Validar_Erro_CPF_EMBRANCO()
+    {
+        var validator = new AdicionarFuncionarioValidator();
+
+        var request = RequestFuncionarioBuilder.Construir();
+        request.CPF = string.Empty;
+
+        var result = validator.Validate(request);
+
+        result.IsValid.Should().BeFalse();
+        result.Errors.Should().ContainSingle().And.Contain(error => error.ErrorMessage.Equals(ResourceMensagensDeErro.CPF_EMBRANCO));
+    }
+
+    [Fact]
+    public void Validar_Erro_RG_EMBRANCO()
+    {
+        var validator = new AdicionarFuncionarioValidator();
+
+        var request = RequestFuncionarioBuilder.Construir();
+        request.RG = string.Empty;
+
+        var result = validator.Validate(request);
+
+        result.IsValid.Should().BeFalse();
+        result.Errors.Should().ContainSingle().And.Contain(error => error.ErrorMessage.Equals(ResourceMensagensDeErro.RG_EMBRANCO));
+    }
+
+    [Fact]
+    public void Validar_Erro_PIS_EMBRANCO()
+    {
+        var validator = new AdicionarFuncionarioValidator();
+
+        var request = RequestFuncionarioBuilder.Construir();
+        request.PIS = string.Empty;
+
+        var result = validator.Validate(request);
+
+        result.IsValid.Should().BeFalse();
+        result.Errors.Should().ContainSingle().And.Contain(error => error.ErrorMessage.Equals(ResourceMensagensDeErro.PIS_EMBRANCO));
+    }
+
+    [Fact]
+    public void Validar_Erro_Cargo_EMBRANCO()
+    {
+        var validator = new AdicionarFuncionarioValidator();
+
+        var request = RequestFuncionarioBuilder.Construir();
+        request.Cargo = string.Empty;
+
+        var result = validator.Validate(request);
+
+        result.IsValid.Should().BeFalse();
+        result.Errors.Should().ContainSingle().And.Contain(error => error.ErrorMessage.Equals(ResourceMensagensDeErro.CARGO_EMBRANCO));
+    }
+
+    [Fact]
+    public void Validar_Erro_Salario_Invalido()
+    {
+        var validator = new AdicionarFuncionarioValidator();
+
+        var request = RequestFuncionarioBuilder.Construir();
+        request.Salario = 0;
+
+        var result = validator.Validate(request);
+
+        result.IsValid.Should().BeFalse();
+        result.Errors.Should().ContainSingle().And.Contain(error => error.ErrorMessage.Equals(ResourceMensagensDeErro.SALARIO_INVALIDO));
+    }
+
+    [Fact]
+    public void Validar_Erro_Rua_EmBranco()
+    {
+        var validator = new EnderecoValidator();
+
+        var request = RequestFuncionarioBuilder.Construir();
+        request.Endereco.Rua = string.Empty;
+
+        var result = validator.Validate(request.Endereco);
+
+        result.IsValid.Should().BeFalse();
+        result.Errors.Should().ContainSingle().And.Contain(error => error.ErrorMessage.Equals(ResourceMensagensDeErro.RUA_EMBRANCO));
+    }
+
+    [Fact]
+    public void Validar_Erro_Numero_EmBranco()
+    {
+        var validator = new EnderecoValidator();
+
+        var request = RequestFuncionarioBuilder.Construir();
+        request.Endereco.Numero = string.Empty;
+
+        var result = validator.Validate(request.Endereco);
+
+        result.IsValid.Should().BeFalse();
+        result.Errors.Should().ContainSingle().And.Contain(error => error.ErrorMessage.Equals(ResourceMensagensDeErro.NUMERO_EMBRANCO));
+    }
+
+    [Fact]
+    public void Validar_Erro_Bairro_EmBranco()
+    {
+        var validator = new EnderecoValidator();
+
+        var request = RequestFuncionarioBuilder.Construir();
+        request.Endereco.Bairro = string.Empty;
+
+        var result = validator.Validate(request.Endereco);
+
+        result.IsValid.Should().BeFalse();
+        result.Errors.Should().ContainSingle().And.Contain(error => error.ErrorMessage.Equals(ResourceMensagensDeErro.BAIRRO_EMBRANCO));
+    }
+
+    [Fact]
+    public void Validar_Erro_Cidade_EmBranco()
+    {
+        var validator = new EnderecoValidator();
+
+        var request = RequestFuncionarioBuilder.Construir();
+        request.Endereco.Cidade = string.Empty;
+
+        var result = validator.Validate(request.Endereco);
+
+        result.IsValid.Should().BeFalse();
+        result.Errors.Should().ContainSingle().And.Contain(error => error.ErrorMessage.Equals(ResourceMensagensDeErro.CIDADE_EMBRANCO));
+    }
+
+    [Fact]
+    public void Validar_Erro_Pais_EmBranco()
+    {
+        var validator = new EnderecoValidator();
+
+        var request = RequestFuncionarioBuilder.Construir();
+        request.Endereco.Pais = string.Empty;
+
+        var result = validator.Validate(request.Endereco);
+
+        result.IsValid.Should().BeFalse();
+        result.Errors.Should().ContainSingle().And.Contain(error => error.ErrorMessage.Equals(ResourceMensagensDeErro.PAIS_EMBRANCO));
+    }
+}
