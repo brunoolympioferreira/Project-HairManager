91c60fdad26ea27975e3e2663dec95c0dc3e3dc8
Bruno Ferreira <bruno.olympio.ferreira@gmail.com>
2023-04-12 19:41:47 -0300

    feat: configurando modulo e componente funcionario

---
+++ src/FrontEnd/HairManager.UI/src/app/funcionario/adicionar-funcionario/adicionar-funcionario.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-adicionar-funcionario',
+  templateUrl: './adicionar-funcionario.component.html',
+  styleUrls: ['./adicionar-funcionario.component.scss']
+})
+export class AdicionarFuncionarioComponent {
+
+}
---
+++ src/FrontEnd/HairManager.UI/src/app/funcionario/funcionario.app.component.ts
@@ -0,0 +1,8 @@
+import { Component } from "@angular/core";
+
+@Component({
+  selector: 'funcionario-app-root',
+  template: '<router-outlet></router-outlet>'
+})
+
+export class FuncionarioAppComponent { }
