2a47eca2f5fbfed5a5839b514b839e02b12fdf9e
Bruno Ferreira <bruno.olympio.ferreira@gmail.com>
2023-06-21 09:05:05 -0300

    feat: implementando configuracao dash funcionarios

--- src/FrontEnd/HairManager.UI/src/app/funcionario/funcionario.route.ts
+++ src/FrontEnd/HairManager.UI/src/app/funcionario/funcionario.route.ts
@@ -2,12 +2,14 @@ import { FuncionarioAppComponent } from './funcionario.app.component';
 import { NgModule } from "@angular/core";
 import { RouterModule, Routes } from "@angular/router";
 import { AdicionarFuncionarioComponent } from './adicionar-funcionario/adicionar-funcionario.component';
+import { ListarFuncionariosComponent } from './listar-funcionarios/listar-funcionarios.component';
 
 const funcionarioRouterConfig: Routes = [
   {
     path: '', component: FuncionarioAppComponent,
     children: [
-      { path: 'adicionar', component: AdicionarFuncionarioComponent }
+      { path: 'adicionar', component: AdicionarFuncionarioComponent },
+      { path: 'dash-funcionarios', component: ListarFuncionariosComponent }
     ]
   }
 ]
---
+++ src/FrontEnd/HairManager.UI/src/app/funcionario/listar-funcionarios/listar-funcionarios.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-listar-funcionarios',
+  templateUrl: './listar-funcionarios.component.html'
+})
+export class ListarFuncionariosComponent {
+
+}
